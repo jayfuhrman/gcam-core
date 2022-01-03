34f38ae4a97c1c9bfa89e15453002c9a903af9cc
Page Kyle <pkyle@pnnl.gov>
2022-01-03 16:26:03 -0700

    further update to hydrogen technology scenarios to ensure no structural supply/demand imbalances

--- input/gcamdata/R/zchunk_batch_hydrogen_dist_scenarios_xml.R
+++ input/gcamdata/R/zchunk_batch_hydrogen_dist_scenarios_xml.R
@@ -12,7 +12,8 @@
 #' original data system was \code{batch_hydrogen.xml.R} (energy XML).
 module_energy_batch_hydrogen_dist_scenarios_xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L225.SubsectorShrwtFllt_h2"))
+    return(c("L225.SubsectorShrwtFllt_h2",
+             "L225.GlobalTechShrwt_h2"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c(XML = "hydrogen_dist_pipeline.xml",
              XML = "hydrogen_dist_liqtruck.xml"))
@@ -22,13 +23,22 @@ module_energy_batch_hydrogen_dist_scenarios_xml <- function(command, ...) {
 
     # Load required inputs
     L225.SubsectorShrwtFllt_h2 <- get_data(all_data, "L225.SubsectorShrwtFllt_h2")
+    L225.GlobalTechShrwt_h2 <- get_data(all_data, "L225.GlobalTechShrwt_h2")
 
     L225.SubsectorShrwtFllt_h2pipe <- subset(L225.SubsectorShrwtFllt_h2,
                                              supplysector == "H2 liquid truck"  | subsector == "H2 liquid truck") %>%
       mutate(share.weight = 0)
 
+    L225.GlobalTechShrwt_h2pipe <- subset(L225.GlobalTechShrwt_h2,
+                                             technology == "H2 liquid truck") %>%
+      mutate(share.weight = 0)
+
     L225.SubsectorShrwtFllt_h2truck <- subset(L225.SubsectorShrwtFllt_h2,
                                               supplysector == "H2 pipeline" | subsector == "H2 pipeline") %>%
+      mutate(share.weight = 0)
+
+    L225.GlobalTechShrwt_h2truck <- subset(L225.GlobalTechShrwt_h2,
+                                          technology == "H2 pipeline") %>%
       mutate(share.weight = 0)
 
     # ===================================================
@@ -36,12 +46,16 @@ module_energy_batch_hydrogen_dist_scenarios_xml <- function(command, ...) {
     # Produce outputs
     hydrogen_dist_pipeline.xml <- create_xml("hydrogen_dist_pipeline.xml") %>%
       add_xml_data(L225.SubsectorShrwtFllt_h2pipe, "SubsectorShrwtFllt") %>%
-      add_precursors("L225.SubsectorShrwtFllt_h2") ->
+      add_xml_data(L225.GlobalTechShrwt_h2pipe, "GlobalTechShrwt") %>%
+      add_precursors("L225.SubsectorShrwtFllt_h2",
+                     "L225.GlobalTechShrwt_h2") ->
       hydrogen_dist_pipeline.xml
 
     hydrogen_dist_liqtruck.xml <- create_xml("hydrogen_dist_liqtruck.xml") %>%
       add_xml_data(L225.SubsectorShrwtFllt_h2truck, "SubsectorShrwtFllt") %>%
-      add_precursors("L225.SubsectorShrwtFllt_h2") ->
+      add_xml_data(L225.GlobalTechShrwt_h2truck, "GlobalTechShrwt") %>%
+      add_precursors("L225.SubsectorShrwtFllt_h2",
+                     "L225.GlobalTechShrwt_h2") ->
       hydrogen_dist_liqtruck.xml
 
     return_data(hydrogen_dist_pipeline.xml, hydrogen_dist_liqtruck.xml)
