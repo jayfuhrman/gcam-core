d4effd75772cc56381c604412fb3e23110ad89c9
RChadsey <roan.chadsey@pnnl.gov>
2025-03-12 10:23:18 -0400

    Updated structure for DACCS cost breakdown

--- input/gcamdata/R/zenergy_L262.dac.R
+++ input/gcamdata/R/zenergy_L262.dac.R
@@ -581,43 +581,76 @@ module_energy_L262.dac <- function(command, ...) {
       rename(sector.name = supplysector, subsector.name = subsector) ->
       L262.GlobalTechCapFac_dac
 
-########## Create a for loop for this section
     # Loop over the ssps
-
-    # Extrapolate non energy cost assumptions to all model years
-    A62.globaltech_cost_ssp2 %>%
-      filter(supplysector == "CO2 removal",
-             subsector == 'dac',
-             technology %in% c('hightemp DAC NG', 'hightemp DAC elec', 'lowtemp DAC heatpump')) %>%
-      gather_years() %>%
-      complete(nesting(supplysector, subsector, technology, minicam.non.energy.input), year = c(year, MODEL_BASE_YEARS, MODEL_FUTURE_YEARS)) %>%
-      arrange(supplysector, year) %>%
-      group_by(supplysector, subsector, technology, minicam.non.energy.input) %>%
-      mutate(value = approx_fun(year, value, rule = 1)) %>%
-      ungroup() %>%
-      filter(year %in% MODEL_YEARS) %>%
-      rename(sector.name = supplysector, subsector.name = subsector) %>%
-      spread(minicam.non.energy.input, value) ->
-      L262.GlobalTech_dac
-
-    # Join non-energy cost variables together
-    L262.GlobalTechNonEnCost_dac_inter <- L262.GlobalTech_dac %>%
-      left_join_error_no_match(L262.GlobalTechCapFac_dac,
-                               by = c("sector.name", "subsector.name", "technology", "year"))%>%
-      mutate(non_energy_cost = (capital.overnight * fixed.charge.rate + OM.fixed)/capacity.factor + OM.var,
-             non_energy_cost_kgC = non_energy_cost * emissions.CONV_C_CO2 * CONV_KG_T * gdp_deflator(1975,2015),
-             minicam.non.energy.input = "non-energy") %>%
-      rename(input.cost = non_energy_cost_kgC) %>%
-      select(sector.name, subsector.name, technology, year, minicam.non.energy.input, input.cost)
-
     L262.GlobalTechCost_dac %>%
-      filter(scenario == "ssp2",
-             !technology %in% c("hightemp DAC elec", "hightemp DAC NG", "lowtemp DAC heatpump")) %>%
-      select(-scenario) %>%
-      bind_rows(L262.GlobalTechNonEnCost_dac_inter) ->
-    L262.GlobalTechNonEnCost_dac
+      filter(scenario %in% c('ssp1', 'ssp2', 'ssp3', 'ssp4', 'ssp5'),
+             !technology %in% c("hightemp DAC elec", "hightemp DAC NG", "lowtemp DAC heatpump")) ->
+      #select(-scenario) %>%
+      #bind_rows(L262.GlobalTechNonEnCost_dac_inter) ->
+      L262.GlobalTechNonEnCost_dac
 
-##########
+    for (ssp in c('ssp1', 'ssp2', 'ssp3', 'ssp4', 'ssp5')) {
+      L262.GlobalTechCost_dac %>%
+        filter(scenario == ssp,
+               sector.name == "CO2 removal",
+               subsector.name == 'dac',
+               technology %in% c('hightemp DAC NG', 'hightemp DAC elec', 'lowtemp DAC heatpump')) %>%
+        #gather_years() %>%
+        #complete(nesting(supplysector, subsector, technology, minicam.non.energy.input), year = c(year, MODEL_BASE_YEARS, MODEL_FUTURE_YEARS)) %>%
+        #arrange(supplysector, year) %>%
+        #group_by(supplysector, subsector, technology, minicam.non.energy.input) %>%
+        #mutate(value = approx_fun(year, value, rule = 1)) %>%
+        #ungroup() %>%
+        #filter(year %in% MODEL_YEARS) %>%
+        #rename(sector.name = supplysector, subsector.name = subsector) %>%
+        spread(minicam.non.energy.input, input.cost) ->
+        L262.GlobalTech_dac
+
+      L262.GlobalTechNonEnCost_dac_inter <- L262.GlobalTech_dac %>%
+        left_join_error_no_match(L262.GlobalTechCapFac_dac,
+                                 by = c("sector.name", "subsector.name", "technology", "year"))%>%
+        mutate(non_energy_cost = (capital.overnight * fixed.charge.rate + OM.fixed)/capacity.factor + OM.var,
+               non_energy_cost_kgC = non_energy_cost * emissions.CONV_C_CO2 * CONV_KG_T * gdp_deflator(1975,2015),
+               minicam.non.energy.input = "non-energy") %>%
+        rename(input.cost = non_energy_cost_kgC) %>%
+        select(sector.name, subsector.name, technology, year, minicam.non.energy.input, input.cost, scenario)
+
+      bind_rows(L262.GlobalTechNonEnCost_dac_inter, L262.GlobalTechNonEnCost_dac) -> L262.GlobalTechNonEnCost_dac
+    }
+
+    # # Extrapolate non energy cost assumptions to all model years
+    # A62.globaltech_cost_ssp2 %>%
+    #   filter(supplysector == "CO2 removal",
+    #          subsector == 'dac',
+    #          technology %in% c('hightemp DAC NG', 'hightemp DAC elec', 'lowtemp DAC heatpump')) %>%
+    #   gather_years() %>%
+    #   complete(nesting(supplysector, subsector, technology, minicam.non.energy.input), year = c(year, MODEL_BASE_YEARS, MODEL_FUTURE_YEARS)) %>%
+    #   arrange(supplysector, year) %>%
+    #   group_by(supplysector, subsector, technology, minicam.non.energy.input) %>%
+    #   mutate(value = approx_fun(year, value, rule = 1)) %>%
+    #   ungroup() %>%
+    #   filter(year %in% MODEL_YEARS) %>%
+    #   rename(sector.name = supplysector, subsector.name = subsector) %>%
+    #   spread(minicam.non.energy.input, value) ->
+    #   L262.GlobalTech_dac
+    #
+    # # Join non-energy cost variables together
+    # L262.GlobalTechNonEnCost_dac_inter <- L262.GlobalTech_dac %>%
+    #   left_join_error_no_match(L262.GlobalTechCapFac_dac,
+    #                            by = c("sector.name", "subsector.name", "technology", "year"))%>%
+    #   mutate(non_energy_cost = (capital.overnight * fixed.charge.rate + OM.fixed)/capacity.factor + OM.var,
+    #          non_energy_cost_kgC = non_energy_cost * emissions.CONV_C_CO2 * CONV_KG_T * gdp_deflator(1975,2015),
+    #          minicam.non.energy.input = "non-energy") %>%
+    #   rename(input.cost = non_energy_cost_kgC) %>%
+    #   select(sector.name, subsector.name, technology, year, minicam.non.energy.input, input.cost)
+    #
+    # L262.GlobalTechCost_dac %>%
+    #   filter(scenario == "ssp2",
+    #          !technology %in% c("hightemp DAC elec", "hightemp DAC NG", "lowtemp DAC heatpump")) %>%
+    #   select(-scenario) %>%
+    #   bind_rows(L262.GlobalTechNonEnCost_dac_inter) ->
+    # L262.GlobalTechNonEnCost_dac
+
     # ===================================================
     # Produce outputs
 
