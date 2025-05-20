476c7a608a609dce0f749540cbb25644a21861a3
RChadsey <roan.chadsey@pnnl.gov>
2025-05-20 13:18:09 -0400

    added automated regionalized link file step to gcamdata

--- input/gcamdata/R/zpolicy_xml_NDCs.R
+++ input/gcamdata/R/zpolicy_xml_NDCs.R
@@ -9,10 +9,13 @@ module_policy_NDCs_xml <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "common/GCAM_region_names",
              FILE = "policy/ghg_constraint_regionalized",
-             FILE = "policy/ghg_link"))
+             FILE = "policy/ghg_link",
+             FILE = "policy/price_adjust",
+             FILE = "policy/demand_adjust"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c(XML = "NDCs_regionalized.xml",
-             XML = "NDCs_global.xml"))
+             XML = "NDCs_global.xml",
+             XML = "NDCs_regionalized_link.xml"))
   } else if(command == driver.MAKE) {
 
 
@@ -23,6 +26,9 @@ module_policy_NDCs_xml <- function(command, ...) {
     ghg_constraint_regionalized <- get_data(all_data, "policy/ghg_constraint_regionalized", strip_attributes = TRUE)
     ghg_link <- get_data(all_data, "policy/ghg_link", strip_attributes = TRUE)
     GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
+    price_adjust <- get_data(all_data, "policy/price_adjust", strip_attributes = TRUE)
+    demand_adjust <- get_data(all_data, "policy/demand_adjust", strip_attributes = TRUE)
+
 
     NDCs_regionalized <- ghg_constraint_regionalized %>%
       tidyr::pivot_longer(cols = -c(year),names_to='market') %>%
@@ -44,6 +50,16 @@ module_policy_NDCs_xml <- function(command, ...) {
       write_to_all_regions(c(LEVEL2_DATA_NAMES[["PortfolioStdConstraint"]]),
                            GCAM_region_names = GCAM_region_names)
 
+    NDCs_regionalized_link <- price_adjust %>%
+      gather_years(value_col = 'price.adjust') %>%
+      left_join(demand_adjust, by = 'linked.ghg.policy') %>%
+      merge(GCAM_region_names) %>%
+      right_join(ghg_link, by = 'region') %>%
+      select(-GCAM_region_ID, -policy.portfolio.standard) %>%
+      mutate('linked.policy' = 'GHG', year.demand.adjust = year, year.price.adjust = year) %>%
+      mutate(demand.adjust = case_when(year == 1975 ~ 0,
+                                       (market == "ROW") & !(linked.ghg.policy %in% c("CO2", "CO2_LUC", "CO2_FUG")) & (year == 2040) ~ 0,
+                                       TRUE ~ demand.adjust))
 
     create_xml("NDCs_regionalized.xml") %>%
       add_xml_data(NDCs_regionalized, "PortfolioStdConstraint") %>%
@@ -57,10 +73,17 @@ module_policy_NDCs_xml <- function(command, ...) {
                      "policy/ghg_link") ->
       NDCs_global.xml
 
+    create_xml("NDCs_regionalized_link.xml") %>%
+      add_xml_data(NDCs_regionalized_link, "GHGConstrLinkNDC") %>%
+      add_precursors("policy/price_adjust",
+                     "policy/demand_adjust") ->
+      NDCs_regionalized_link.xml
+
 
 
   return_data(NDCs_regionalized.xml,
-              NDCs_global.xml)
+              NDCs_global.xml,
+              NDCs_regionalized_link.xml)
 
   } else {
     stop("Unknown command")
