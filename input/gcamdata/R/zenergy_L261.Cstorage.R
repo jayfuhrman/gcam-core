6077f35168e2db05de65c3c33404a092f6990523
fuhr472 <jay.fuhrman@pnnl.gov>
2025-10-29 15:23:10 -0400

    Address invalid grades in regions with very small dynamic CCS supply

--- input/gcamdata/R/zenergy_L261.Cstorage.R
+++ input/gcamdata/R/zenergy_L261.Cstorage.R
@@ -230,7 +230,20 @@ module_energy_L261.Cstorage <- function(command, ...) {
       mutate(available = max_CO2_injection * fraction  * USA_max_CCS_rate_NETL / USA_OG_volume_MTCO2, #scale injectivity back to US NETL data.  The result will be a supply curve that exactly matches NETL for USA, with other regions scaled based on relative O&G peak production volumes
              available = round(available,energy.DIGITS_RESOURCE),
              extractioncost = round(extractioncost,energy.DIGITS_COST)) %>%
-      select(region, renewresource = resource, sub.renewable.resource = subresource, grade, available, extractioncost)
+      select(region, renewresource = resource, sub.renewable.resource = subresource, grade, available, extractioncost) %>%
+      group_by(region,renewresource,sub.renewable.resource) %>%
+      arrange(available, .by_group = TRUE) %>%
+      #Rounding was leading to some invalid grades at the top and bottom of supply curves in regions with very small supply.
+      #We identify those cases here and recalculate to ensure smooth monotonically increasing supply curves
+      mutate(prev = lag(available),
+             nxt = lead(available),
+             invalid_grade = available <= lag(available, default = first(available)),
+             invalid_grade = if_else(grade == "grade 0", FALSE, invalid_grade),
+             available = if_else((invalid_grade == TRUE & prev == 0), (prev + nxt / 2), available),
+             available = if_else((invalid_grade == TRUE & available == max(available)), available * 10, available),
+             available = round(available,energy.DIGITS_RESOURCE)) %>%
+      ungroup() %>%
+      select(LEVEL2_DATA_NAMES[["GrdRenewRsrcCurves"]])
     #construct a supply curve based on fractions from NETL's saline storage cost model for the U.S. and then apply these fractions to max CO2 injectivity based on O&G volumetric flow rates
 
     k_rapid = 0.24 #shale gas growth in USA (EIA)
