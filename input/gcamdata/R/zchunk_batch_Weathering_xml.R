4ba636c7df85f7aee8c78f87c988802601215904
fuhr472 <jay.fuhrman@pnnl.gov>
2025-11-05 14:36:13 -0500

    Clean ups to ERW rounding and xml structure

--- input/gcamdata/R/zchunk_batch_Weathering_xml.R
+++ input/gcamdata/R/zchunk_batch_Weathering_xml.R
@@ -89,6 +89,7 @@ module_energy_batch_Weathering_xml <- function(command, ...) {
       Weathering.xml
 
     create_xml("Weathering_high.xml") %>%
+      add_node_equiv_xml("technology") %>%
       add_xml_data(L263.StubTechEff %>%
                      filter(scenario == 'rapid_growth_rate'),"StubTechEff") %>%
       add_xml_data(L263.TechPmult %>%
@@ -97,6 +98,7 @@ module_energy_batch_Weathering_xml <- function(command, ...) {
       Weathering_high.xml
 
     create_xml("Weathering_med.xml") %>%
+      add_node_equiv_xml("technology") %>%
       add_xml_data(L263.StubTechEff %>%
                      filter(scenario == 'medium_growth_rate'),"StubTechEff") %>%
       add_xml_data(L263.TechPmult %>%
@@ -105,6 +107,7 @@ module_energy_batch_Weathering_xml <- function(command, ...) {
       Weathering_med.xml
 
     create_xml("Weathering_low.xml") %>%
+      add_node_equiv_xml("technology") %>%
       add_xml_data(L263.StubTechEff %>%
                      filter(scenario == 'slow_growth_rate'),"StubTechEff") %>%
       add_xml_data(L263.TechPmult %>%
