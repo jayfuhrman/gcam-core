c06e82cd7939ddb1fb92574dd40aae395272e71a
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2026-01-02 16:30:45 -0500

    Round before substituting zero efficiency values to avoid NA errors

--- input/gcamdata/R/zenergy_L261.Cstorage.R
+++ input/gcamdata/R/zenergy_L261.Cstorage.R
@@ -324,8 +324,8 @@ module_energy_L261.Cstorage <- function(command, ...) {
              stub.technology = 'ccs dynamic-capacity',
              minicam.energy.input = 'carbon-storage dynamic',
              market.name = region) %>%
-      mutate(efficiency = if_else(efficiency == 0, 0.001,efficiency),
-             efficiency = round(efficiency,energy.DIGITS_EFFICIENCY)) %>%
+      mutate(efficiency = round(efficiency,energy.DIGITS_EFFICIENCY),
+             efficiency = if_else(efficiency == 0, 0.001,efficiency)) %>%
       select(c('scenario',LEVEL2_DATA_NAMES[['StubTechEff']]))
 
     L261.TechPmult <- L261.StubTechEff %>%
