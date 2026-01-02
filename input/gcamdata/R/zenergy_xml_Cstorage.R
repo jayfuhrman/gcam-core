2eeacc76090e5d0c04fa772b28233c9cd40f5614
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2026-01-02 13:50:09 -0500

    Leave Cstorage.xml unmodified

--- input/gcamdata/R/zenergy_xml_Cstorage.R
+++ input/gcamdata/R/zenergy_xml_Cstorage.R
@@ -79,14 +79,6 @@ module_energy_Cstorage_xml <- function(command, ...) {
     create_xml("Cstorage.xml") %>%
       add_xml_data(L261.Rsrc, "Rsrc") %>%
       add_xml_data(L261.UnlimitRsrc, "UnlimitRsrc") %>%
-      #add_node_equiv_xml("resource") %>%
-      add_node_equiv_xml("subresource") %>%
-      add_node_equiv_xml("technology") %>%
-      add_xml_data(L261.ResSubresourceProdLifetime, "ResSubresourceProdLifetime") %>%
-      add_xml_data(L261.ResReserveTechDeclinePhase, "ResReserveTechDeclinePhase") %>%
-      add_xml_data(L261.ResReserveTechProfitShutdown, "ResReserveTechProfitShutdown") %>%
-      add_xml_data(L261.ResReserveTechLifetime, "ResReserveTechLifetime") %>%
-      add_xml_data(L261.ResReserveTechInvestmentInput, "ResReserveTechInvestmentInput") %>%
       add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
       add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
       add_logit_tables_xml(L261.Supplysector_C, "Supplysector") %>%
@@ -119,8 +111,8 @@ module_energy_Cstorage_xml <- function(command, ...) {
       add_xml_data(L261.ResReserveTechProfitShutdown, "ResReserveTechProfitShutdown") %>%
       add_xml_data(L261.ResReserveTechLifetime, "ResReserveTechLifetime") %>%
       add_xml_data(L261.ResReserveTechInvestmentInput, "ResReserveTechInvestmentInput") %>%
-      #add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
-      #add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
+      add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
+      add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
       add_xml_data(L261.DynamicResTechShrwt_C, "ResTechShrwt") %>%
       add_logit_tables_xml(L261.Supplysector_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "Supplysector") %>%
       add_logit_tables_xml(L261.SubsectorLogit_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "SubsectorLogit") %>%
@@ -157,8 +149,8 @@ module_energy_Cstorage_xml <- function(command, ...) {
       add_xml_data(L261.ResReserveTechProfitShutdown, "ResReserveTechProfitShutdown") %>%
       add_xml_data(L261.ResReserveTechLifetime, "ResReserveTechLifetime") %>%
       add_xml_data(L261.ResReserveTechInvestmentInput, "ResReserveTechInvestmentInput") %>%
-      #add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
-      #add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
+      add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
+      add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
       add_xml_data(L261.DynamicResTechShrwt_C, "ResTechShrwt") %>%
       add_logit_tables_xml(L261.Supplysector_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "Supplysector") %>%
       add_logit_tables_xml(L261.SubsectorLogit_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "SubsectorLogit") %>%
@@ -196,8 +188,8 @@ module_energy_Cstorage_xml <- function(command, ...) {
       add_xml_data(L261.ResReserveTechProfitShutdown, "ResReserveTechProfitShutdown") %>%
       add_xml_data(L261.ResReserveTechLifetime, "ResReserveTechLifetime") %>%
       add_xml_data(L261.ResReserveTechInvestmentInput, "ResReserveTechInvestmentInput") %>%
-      #add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
-      #add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
+      add_xml_data(L261.RsrcCurves_C, "RsrcCurves") %>%
+      add_xml_data(L261.ResTechShrwt_C, "ResTechShrwt") %>%
       add_xml_data(L261.DynamicResTechShrwt_C, "ResTechShrwt") %>%
       add_logit_tables_xml(L261.Supplysector_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "Supplysector") %>%
       add_logit_tables_xml(L261.SubsectorLogit_C %>% filter(supplysector != energy.CO2.STORAGE.MARKET), "SubsectorLogit") %>%
