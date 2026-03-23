692d5b98c860792641b029d608a1c7a2578ba42c
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2026-03-23 17:16:23 -0400

    correct growth rate for med scenario

--- input/gcamdata/R/zenergy_xml_Cstorage.R
+++ input/gcamdata/R/zenergy_xml_Cstorage.R
@@ -150,7 +150,7 @@ module_energy_Cstorage_xml <- function(command, ...) {
       add_node_equiv_xml("resource") %>%
       add_node_equiv_xml("subresource") %>%
       add_node_equiv_xml("technology") %>%
-      add_xml_data(L261.CStorageCurvesDynamic %>% filter(scenario == 'rapid growth rate'), "GrdRenewRsrcCurves") %>%
+      add_xml_data(L261.CStorageCurvesDynamic %>% filter(scenario == 'medium growth rate'), "GrdRenewRsrcCurves") %>%
       add_xml_data(L261.DynamicCstorageRsrcMax, "GrdRenewRsrcMax") %>%
       add_xml_data(L261.ResSubresourceProdLifetime, "ResSubresourceProdLifetime") %>%
       add_xml_data(L261.ResReserveTechDeclinePhase, "ResReserveTechDeclinePhase") %>%
