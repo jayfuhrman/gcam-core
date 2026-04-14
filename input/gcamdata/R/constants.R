7ee1e1421115cb7b934b3542602d4a705a21f2d0
jayfuhrman <48569483+jayfuhrman@users.noreply.github.com>
2026-04-13 20:20:03 -0400

    hard code to 75 to accomidate when 2025 becomes a projection year

--- input/gcamdata/R/constants.R
+++ input/gcamdata/R/constants.R
@@ -511,7 +511,7 @@ aglu.DIGITS_WATER_CONTENT <- 2
 
 km2.to.ha <- 100 # From  km2 to ha
 biochar.rate <- 20 # Assumed applicaiton rate of 10 tons of biochar per acre, in line with literature
-assumed.biochar.years <- max(MODEL_FUTURE_YEARS) - min(MODEL_FUTURE_YEARS) # biochar application begins in first projection period
+assumed.biochar.years <- 75 # biochar application begins in first projection period (2025)
 
 
 
